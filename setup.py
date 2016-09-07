650a174d59bc4449d2200ec9566d87393cc803eb
andrewmontes87 <andrewmontes87@gmail.com>
2016-09-06 18:29:06 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='scrapy_og',
+    version='1.0.0',
+    description='Scrapy spiders for oil gas visualizer',
+    author='Andrew Montes',
+    author_email='andrewmontes87@gmail.com',    
+    long_description=open('README.md').read(),
+)
